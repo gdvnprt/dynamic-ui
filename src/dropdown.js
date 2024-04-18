67b02de0d2a85825fa727714db50fa8632e2b420
Gavin Davenport <gavinkdavenport@gmail.com>
2024-04-18 16:59:11 -0700

    Build dropdown transition
    dropdown menu expands and collapses based on hover

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,3 +1,14 @@
-export default function dropdown(element){
-    element.toggleClass('visible');
+export default function dropdown(element, selectorOne, selectorTwo){
+    element.addEventListener('mouseover', () => {
+        element.classList.toggle(selectorOne);
+    });
+    element.addEventListener('mouseover', () => {
+        element.classList.toggle(selectorTwo);
+    });
+    element.addEventListener('mouseout', () => {
+        element.classList.toggle(selectorOne);
+    });
+    element.addEventListener('mouseout', () => {
+        element.classList.toggle(selectorTwo);
+    });
 };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import  previous from './slider.js';
 import next from './slider.js';
 import './style.css';
 
-console.log('Webpack running');
+const dropMenu = document.querySelector('#nav');
+
+dropdown(dropMenu, "drop-down", "collapsed");
